d19f4d0ebf40b77104c14b36044d7b8e9e84cb0f
Virgilio Nayal <VNL6941R@externo.alisedainmobiliaria.com>
2024-06-07 14:21:33 +0200

    Implement Home Page Layout

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,28 @@
+import HeaderBox from "@/components/HeaderBox";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
 import React from "react";
 
 const Home = () => {
-	return <div>Home</div>;
+  const loggedIn = { firstName: "Vir" };
+  return (
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || "Invitado"}
+            subtext="Inicia sesión y administra tu cuenta y transfiere dinero eficazmente"
+          />
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={12200.1}
+          />
+        </header>
+      </div>
+    </section>
+  );
 };
 
 export default Home;
